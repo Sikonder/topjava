9f9e83810e9952f87c35a148906c85c44668ad8d
Sikonder <Sikonder007@yandex.ru>
2017-11-05 11:01:29 +0300

    Prepared for editing

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
