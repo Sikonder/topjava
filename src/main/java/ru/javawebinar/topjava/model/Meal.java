4b700e56965d5e7f8f178a25358eac732e8b7acf
Sikonder <Sikonder007@yandex.ru>
2017-11-09 11:07:45 +0300

    HW01 is checked

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal {
+    private Integer id;
+
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -12,13 +14,7 @@ public class Meal {
     private final int calories;
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
-        this.dateTime = dateTime;
-        this.description = description;
-        this.calories = calories;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
+        this(null, dateTime, description, calories);
     }
 
     public String getDescription() {
@@ -29,14 +25,43 @@ public class Meal {
         return calories;
     }
 
-    public LocalDate getDate() {
-        return dateTime.toLocalDate();
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
     }
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
     }
+    public LocalDate getDate() {
+        return dateTime.toLocalDate();
+    }
 
+    public boolean isNew() {
+        return id == null;
+    }
 
-
+    @Override
+    public String toString() {
+        return "Meal{" +
+                "id=" + id +
+                ", dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                '}';
+    }
 }
