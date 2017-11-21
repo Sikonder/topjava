08f78e1d5754915b5f45e1b0d4a2a5b8aa4e471b
Sikonder <Sikonder007@yandex.ru>
2017-11-21 20:59:52 +0300

    Added Init and population of Meals

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
+
 
 CREATE TABLE users
 (
@@ -23,3 +25,14 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+  id INT PRIMARY KEY NOT NULL,
+  user_id INTEGER NOT NULL,
+  description VARCHAR NOT NULL,
+  calories INT NOT NULL,
+  dateTime TIMESTAMP NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (id, user_id, description, calories, datetime) VALUES
+  (100002, 100000, 'Завтрак', 500, '2017-11-21 10:00:00'),
+  (100003, 100000, 'Обед', 700, '2017-11-21 13:00:00'),
+  (100004, 100000, 'Ужин', 500, '2017-11-21 20:00:00'),
+  (100005, 100001, 'Завтрак', 750, '2017-11-21 10:00:00'),
+  (100006, 100001, 'Обед', 750, '2017-11-21 13:00:00'),
+  (100007, 100001, 'Ужин', 550, '2017-11-21 20:00:00');
