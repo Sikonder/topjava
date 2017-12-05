388a8747abe4a9ca2d256d8d6b95b27f53c1725b
Sikonder <Sikonder007@yandex.ru>
2017-12-05 23:12:57 +0300

    HW05 is completed

---
+++ src/test/java/ru/javawebinar/topjava/service/JpaUserServiceClass.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles({"postgres", "jpa"})
+public class JpaUserServiceClass extends UserServiceTest{
+}
